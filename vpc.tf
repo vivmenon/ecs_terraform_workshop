fbfcce11414dd6d1eaab721b895b8fa05aa0dd9f
vivmenon <52816936+vivmenon@users.noreply.github.com>
2021-07-16 10:17:10 +0100

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -7,7 +7,7 @@ module "vpc" {
   public_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   tags = {
     "env"       = "dev"
-    "createdBy" = "mkerimova"
+    "createdBy" = "vmn"
   }
 
 }
